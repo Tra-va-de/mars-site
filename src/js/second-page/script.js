2626313c99ebc1f901f9eccd75f812198e62b3a3
Tra-va-de <distraction.mansion@gmail.com>
2023-04-28 14:24:29 +0400

    Поправили возвращение элемента в центр кадра

--- src/js/second-page/script.js
+++ src/js/second-page/script.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     elements.forEach(element => {
         element.addEventListener('mouseover', () => {
-            console.log("*** Стартуем ***");
-            console.log("Имя элемента: ", element.querySelector('.professions__name').innerHTML);
+            // console.log("*** Стартуем ***");
+            // console.log("Имя элемента: ", element.querySelector('.professions__name').innerHTML);
 
             // получаем анимацию элемента
             const animation = element.getAnimations()[0];
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // берем все кадры анимации
             const keyframes = animation.effect.getKeyframes();
-            
+
             // получаем координа координату центра элемента по оси x
             const rect = element.getBoundingClientRect();
             const elementCenter = (rect.left + rect.right) / 2 + window.scrollX;
 
-            console.log('elementCenter:', elementCenter, 'screenCenter: ', screenCenter);
+            // console.log('elementCenter:', elementCenter, 'screenCenter: ', screenCenter);
 
             if (elementCenter < screenCenter) {
                 elements.forEach(element => {
@@ -30,7 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
             }
 
-            console.log("Время старта: ", animation.startTime);
+            // console.log("Время старта: ", animation.startTime);
+
             // проверяем, что анимация запущена
             if (animation.startTime !== null) {
                 // получаем текущие вычисленные значения таймингов анимации          
@@ -47,39 +48,69 @@ document.addEventListener('DOMContentLoaded', function () {
                 // и переменную для разницы с настоящим (текущим) временем
                 let closestDistance = Infinity;
 
+                // переменная для времени ожидания
+                let waitingTime = 0;
+
                 // перебираем все кадры анимации
                 for (let i = 0; i < keyframes.length; i++) {
                     // получаем время до текущего кадра
                     const offset = keyframes[i].offset * duration;
                     // получаем разницу между текущим временем анимации и временем кадра
                     const distance = Math.abs(currentTime - offset);
-                    // проверяем, что разница во времени с данным кадром меньше предыдущего сохраненного значения,
-                    // а также больше нуля
-                    console.log("до кадра", keyframes[i].offset, "distance = ", distance);
-                    if (distance < closestDistance && offset >= currentTime) {
-                    // if (offset > currentTime) {
-                        // сохраняем время ожидания до ближайшего кадра
-                        closestDistance = offset - currentTime;
-                        // и время до ближайшего кадра со старта
-                        closestOffset = offset;
+
+                    // console.log("до кадра", keyframes[i].offset, "distance = ", distance);
+
+                    if (animation.playbackRate === 1) {
+                        // проверяем, 
+                        // что разница во времени с данным кадром меньше предыдущего сохраненного значения,
+                        // а также, что время до кадра больше или равно времени со старта
+                        if (distance < closestDistance && offset >= currentTime) {
+                            // сохраняем время ожидания до ближайшего кадра
+                            // время до текущего кадра - время с момента старта итерации
+                            closestDistance = offset - currentTime;
+                            // и время до ближайшего кадра со старта
+                            closestOffset = offset;
+                            // сохраняем время ожидания
+                            // время до ближайшего кадра со старта - времяс момента старта итерации
+                            waitingTime = closestOffset - currentTime;
+
+                            // console.log(closestOffset, waitingTime);
+                        }
+                    } else {
+                        // проверяем, что 
+                        // разница во времени с данным кадром меньше предыдущего сохраненного значения,
+                        // а также, что время до кадра меньше или равно времени со старта
+                        if (distance < closestDistance && offset <= currentTime) {
+                            // сохраняем время ожидания до ближайшего кадра
+                            // время с момента старта итерации - время до текущего кадра
+                            closestDistance = currentTime - offset;
+                            // и время до ближайшего кадра со старта
+                            closestOffset = offset;
+                            // сохраняем время ожидания
+                            waitingTime = currentTime - closestOffset;
+
+                            // console.log(closestDistance, waitingTime);
+                        }
                     }
                 }
-                console.log("Разница с настоящим (текущим) временем: ", closestDistance);
+                // console.log("Разница с настоящим (текущим) временем: ", closestDistance);
 
                 elements.forEach(element => {
                     const animation = element.getAnimations()[0];
+
+                    // console.log(closestOffset, currentTime, closestOffset - currentTime);
+
                     // Дожидаемся ближайшего кадра
-                    console.log(closestOffset, currentTime, closestOffset - currentTime);
                     setTimeout(() => {
                         animation.playbackRate = 1;
                         animation.pause();
                         // увеличиваем немного выделенный элемент
                         container.style.paddingLeft = "5%";
                         element.style.flexBasis = "100%";
-                    }, closestOffset - currentTime);
+                    }, waitingTime);
                 });
 
-                console.log("*** Готово ***");
+                // console.log("*** Готово ***");
             }
         });
 
@@ -91,7 +122,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 // уменьшаем обратно выделенный элемент
                 container.style.paddingLeft = "10%";
                 element.style.flexBasis = "90%";
-                console.log("Добби свободен");
+
+                // console.log("Добби свободен");
             });
         });
     });
