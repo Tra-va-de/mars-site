8c56c72e3a8391eac961fdf6c2fe40a22f2a134a
Tra-va-de <distraction.mansion@gmail.com>
2023-12-12 20:59:20 +0400

    Сделали слайдер на второй странице.

--- src/js/second-page/script.js
+++ src/js/second-page/script.js
@@ -1,130 +1,122 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const container = document.querySelector('.professions__cards');
-    const elements = document.querySelectorAll('.professions__card');
+const swiper = new Swiper('.swiper', {
+    // ====================
+    // Основные параметры
+    // ====================
+    // направление
+    direction: 'horizontal',
+    // зацикленный слайдер
+    loop: true,
+    // скорость переключения
+    speed: 1000,
 
-    const windowWidth = window.innerWidth;
-    const screenCenter = windowWidth / 2;
+    // кол-во отображаемых слайдов
+    slidesPerView: 3,
+    // расстояние между слайдами
+    spaceBetween: 130,
 
-    elements.forEach(element => {
-        element.addEventListener('mouseover', () => {
-            // console.log("*** Стартуем ***");
-            // console.log("Имя элемента: ", element.querySelector('.professions__name').innerHTML);
+    // активный слайд по центру
+    // centeredSlides: true,
 
-            // получаем анимацию элемента
-            const animation = element.getAnimations()[0];
-            // console.log(animation);
+    // прокрутка мышью
+    mousewheel: true,
+    // курсор с рукой
+    grabCursor: true,
 
-            // берем все кадры анимации
-            const keyframes = animation.effect.getKeyframes();
+    // прокрутка клавиатурой
+    keyboard: {
+        enabled: true,
+        // только в пределах видимости
+        onlyInViewport: true,
+    },
 
-            // получаем координа координату центра элемента по оси x
-            const rect = element.getBoundingClientRect();
-            const elementCenter = (rect.left + rect.right) / 2 + window.scrollX;
+    // автослайдинг
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
 
-            // console.log('elementCenter:', elementCenter, 'screenCenter: ', screenCenter);
+    // ====================
+    // Адаптив
+    // ====================
+    breakpoints: {
+        1600: {
+            spaceBetween: 130,
+        },
 
-            if (elementCenter < screenCenter) {
-                elements.forEach(element => {
-                    const animation = element.getAnimations()[0];
-                    animation.playbackRate = -1;
-                });
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+
+        0: {
+            slidesPerView: 1,
+        },
+    },
+});
+
+
+// настройка слайдов и взаимодействия с ними
+swiper.slides.forEach((slide, index) => {
+    // Получим высоту слайда
+    const cardHeight = slide.clientHeight;
+    // Установим явную высоту слайда
+    slide.style.height = cardHeight + 'px';
+
+    // дополнительные настройки при наведении курсора
+    slide.addEventListener('mouseenter', () => {
+        // Остановим автоматическую прокрутку слайдов при наведении курсора
+        swiper.autoplay.stop();
+    });
+
+    slide.addEventListener('mouseleave', () => {
+        // Запустим автоматическую прокрутку слайдов при уходе курсора
+        swiper.autoplay.start();
+    });
+
+    // настройка клика по слайду
+    slide.addEventListener('click', () => {
+        // Проверим, отображается ли больше одного слайда
+        if (swiper.params.slidesPerView > 1) {
+            // Если текущий слайд активен, перейдем на предыдущий слайд
+            if (slide.classList.contains('swiper-slide-active')) {
+                swiper.slidePrev(300);
             }
-
-            // console.log("Время старта: ", animation.startTime);
-
-            // проверяем, что анимация запущена
-            if (animation.startTime !== null) {
-                // получаем текущие вычисленные значения таймингов анимации          
-                const computedTiming = animation.effect.getComputedTiming();
-                // получаем время анимации
-                const duration = computedTiming.duration;
-                // получаем текущую итерацию (повтор)
-                const currentIteration = computedTiming.currentIteration;
-                // вычисляем сколько времени прошло с момента старта итерации
-                const currentTime = computedTiming.localTime - duration * currentIteration;
-
-                // объявляем переменную для времени ближайшего кадра от начала отсчёта  
-                let closestOffset = 0;
-                // и переменную для разницы с настоящим (текущим) временем
-                let closestDistance = Infinity;
-
-                // переменная для времени ожидания
-                let waitingTime = 0;
-
-                // перебираем все кадры анимации
-                for (let i = 0; i < keyframes.length; i++) {
-                    // получаем время до текущего кадра
-                    const offset = keyframes[i].offset * duration;
-                    // получаем разницу между текущим временем анимации и временем кадра
-                    const distance = Math.abs(currentTime - offset);
-
-                    // console.log("до кадра", keyframes[i].offset, "distance = ", distance);
-
-                    if (animation.playbackRate === 1) {
-                        // проверяем, 
-                        // что разница во времени с данным кадром меньше предыдущего сохраненного значения,
-                        // а также, что время до кадра больше или равно времени со старта
-                        if (distance < closestDistance && offset >= currentTime) {
-                            // сохраняем время ожидания до ближайшего кадра
-                            // время до текущего кадра - время с момента старта итерации
-                            closestDistance = offset - currentTime;
-                            // и время до ближайшего кадра со старта
-                            closestOffset = offset;
-                            // сохраняем время ожидания
-                            // время до ближайшего кадра со старта - времяс момента старта итерации
-                            waitingTime = closestOffset - currentTime;
-
-                            // console.log(closestOffset, waitingTime);
-                        }
-                    } else {
-                        // проверяем, что 
-                        // разница во времени с данным кадром меньше предыдущего сохраненного значения,
-                        // а также, что время до кадра меньше или равно времени со старта
-                        if (distance < closestDistance && offset <= currentTime) {
-                            // сохраняем время ожидания до ближайшего кадра
-                            // время с момента старта итерации - время до текущего кадра
-                            closestDistance = currentTime - offset;
-                            // и время до ближайшего кадра со старта
-                            closestOffset = offset;
-                            // сохраняем время ожидания
-                            waitingTime = currentTime - closestOffset;
-
-                            // console.log(closestDistance, waitingTime);
-                        }
-                    }
-                }
-                // console.log("Разница с настоящим (текущим) временем: ", closestDistance);
-
-                elements.forEach(element => {
-                    const animation = element.getAnimations()[0];
-
-                    // console.log(closestOffset, currentTime, closestOffset - currentTime);
-
-                    // Дожидаемся ближайшего кадра
-                    setTimeout(() => {
-                        animation.playbackRate = 1;
-                        animation.pause();
-                        // увеличиваем немного выделенный элемент
-                        container.style.paddingLeft = "5%";
-                        element.style.flexBasis = "100%";
-                    }, waitingTime);
-                });
-
-                // console.log("*** Готово ***");
+            // Если не активен, и индекс не равен 0, перейдем на предыдущий слайд
+            else if (index != 0) {
+                swiper.slideToLoop(index - 1, 300);
             }
-        });
+            // Иначе, если индекс равен 0, перейдем на последний слайд
+            else {
+                swiper.slideToLoop(swiper.slides.length - 1, 300);
+            }
+        }
+    });
+});
 
-        element.addEventListener('mouseout', () => {
-            elements.forEach(element => {
-                const animation = element.getAnimations()[0];
-                animation.playbackRate = 1;
-                animation.play();
-                // уменьшаем обратно выделенный элемент
-                container.style.paddingLeft = "10%";
-                element.style.flexBasis = "90%";
 
-                // console.log("Добби свободен");
-            });
-        });
+// Получаем все элементы <details> внутри слайдов
+const detailsElements = document.querySelectorAll('.swiper-slide details');
+
+// Добавляем обработчик события toggle каждому элементу <details>
+detailsElements.forEach(function (details) {
+    // Находим родительский слайд
+    const slide = details.closest('.swiper-slide');
+    const cardHeight = slide.clientHeight;
+
+    details.addEventListener('toggle', function () {
+        // Если слайд найден, обновляем его размер
+        if (slide) {
+            // Если <details> открыт, получаем высоту его содержимого и обновляем высоту слайда
+            if (details.open) {
+                const detailsHeight = slide.querySelector('.professions__details').clientHeight;
+                slide.style.height = detailsHeight + 'px';
+            } 
+            // Если <details> закрыт, восстанавливаем исходную высоту слайда
+            else {
+                // Восстанавливаем исходную высоту слайда
+                slide.style.height = cardHeight + 'px';
+            }
+        };
     });
 });
